411533410974b3d56d3e214b5759555ff7bcd988
PureTilt <kirostat@gmail.com>
2022-01-12 11:56:10 +0500

    1.4.2a
    vila fix

--- VIC/jars/src/data/scripts/weapons/vic_vila_script.java
+++ VIC/jars/src/data/scripts/weapons/vic_vila_script.java
@@ -121,7 +121,7 @@ public class vic_vila_script implements EveryFrameWeaponEffectPlugin, OnFireEffe
 
     @Override
     public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
-        if (weapon == null || weapon.getShip() == null) return;
+        if (weapon == null || weapon.getShip() == null || weapon.getShip().getWeaponGroupFor(weapon) == null || weapon.getShip().getWeaponGroupFor(weapon).getAutofirePlugin(weapon) == null) return;
         final float startingSpeed = MathUtils.getRandomNumberInRange(0.25f, 0.5f);
         final float rangeBeforeCurving = MathUtils.getRandomNumberInRange(0.25f, 0.5f);
 
