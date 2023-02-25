0a9c257c503c520ff5774f0c4c9a46bc4862e420
PureTilt <kirostat@gmail.com>
2023-02-25 15:32:58 +0500

    Hellfire changes

---
+++ jars/src/data/hullmods/vic_hellStormV3.java
@@ -0,0 +1,41 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+
+public class vic_hellStormV3 extends BaseHullMod {
+
+    final float bonus = 1.5f,
+                OPIncrease = 2f;
+
+    public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+        stats.getEnergyWeaponDamageMult().modifyMult(id, bonus);
+        stats.getEnergyWeaponFluxCostMod().modifyMult(id, bonus);
+        stats.getBallisticWeaponDamageMult().modifyMult(id, bonus);
+        stats.getBallisticWeaponFluxCostMod().modifyMult(id, bonus);
+        stats.getMissileAmmoBonus().modifyMult(id, bonus);
+        stats.getMissileAmmoRegenMult().modifyMult(id, bonus);
+        stats.getDynamic().getMod("small_missile_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("medium_missile_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("large_missile_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("small_ballistic_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("medium_ballistic_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("large_ballistic_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("small_energy_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("medium_energy_mod").modifyMult(id, OPIncrease);
+        stats.getDynamic().getMod("large_energy_mod").modifyMult(id, OPIncrease);
+    }
+
+
+    public String getDescriptionParam(int index, HullSize hullSize) {
+        if (index == 0) return "" + (int) ((bonus - 1) * 100) + "%";
+        if (index == 1) return "" + (int) ((bonus - 1) * 100) + "%";
+        if (index == 2) return "" + (int) ((OPIncrease - 1) * 100) + "%";
+        return null;
+    }
+
+    public boolean affectsOPCosts() {
+        return true;
+    }
+}
