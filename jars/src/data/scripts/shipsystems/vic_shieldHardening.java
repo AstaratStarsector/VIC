13e7b21959631e3eb37850bcc48ef164f86c2cd4
PureTilt <kirostat@gmail.com>
2023-03-20 14:16:44 +0500

    fortress fix

--- jars/src/data/scripts/shipsystems/vic_shieldHardening.java
+++ jars/src/data/scripts/shipsystems/vic_shieldHardening.java
@@ -32,13 +32,36 @@ public class vic_shieldHardening extends BaseShipSystemScript {
             ship.addListener(new DamageTakenModifier() {
                 @Override
                 public String modifyDamageTaken(Object param, CombatEntityAPI target, DamageAPI damage, Vector2f point, boolean shieldHit) {
+                    if (currentOverShield <= 0) return null;
                     if (target instanceof ShipAPI){
                         if (ship.getSystem().isStateActive() && ship.getFluxLevel() > 0.5f){
                             float absorptionRate = Math.max(0,Math.min(1,(ship.getFluxLevel() - 0.5f) * 2.5f)) * 0.75f * ship.getSystem().getEffectLevel();
-                            float damagePrevented = damage.getDamage() * absorptionRate;
+                            float actualDamage = damage.computeDamageDealt(Global.getCombatEngine().getElapsedInLastFrame());
+                            switch (damage.getType()){
+                                case KINETIC:
+                                    actualDamage *=2;
+                                    break;
+                                case HIGH_EXPLOSIVE:
+                                    actualDamage *=0.5;
+                                    break;
+                                case FRAGMENTATION:
+                                    actualDamage *=0.25;
+                                    break;
+                            }
+                            /*
+                            actualDamage *= ship.getMutableStats().getShieldDamageTakenMult().getModifiedValue();
+                            actualDamage *= ship.getMutableStats().getShieldAbsorptionMult().getModifiedValue();
+
+                             */
+                            float damagePrevented = actualDamage * absorptionRate;
                             float fluxPerDamage = ship.getShield().getFluxPerPointOfDamage() * ship.getMutableStats().getShieldDamageTakenMult().getModifiedValue();
                             currentOverShield -= damagePrevented * fluxPerDamage;
-                            Global.getLogger(vic_shieldHardening.class).info(damagePrevented + "\\" + fluxPerDamage);
+                            if (damagePrevented >= currentOverShield){
+                                absorptionRate = currentOverShield / damage.getDamage();
+                                damagePrevented = currentOverShield;
+                                currentOverShield = 0;
+                            }
+                            //Global.getLogger(vic_shieldHardening.class).info(damage.getDamage() + "\\" + actualDamage + "\\" + damage.getDpsDuration());
                             damage.getModifier().modifyMult("vic_fortressProtocol", 1 - absorptionRate);
                             return "vic_fortressProtocol";
                         }
@@ -73,7 +96,7 @@ public class vic_shieldHardening extends BaseShipSystemScript {
         stats.getEnergyRoFMult().modifyMult(id, 1f - weaponRoFReduction * effectLevel);
 
         float absorptionRate = Math.max(0,Math.min(1,(ship.getFluxLevel() - 0.5f) * 2.5f)) * 0.75f;
-        if (Global.getCombatEngine().getPlayerShip() == ship){
+        if (Global.getCombatEngine().getPlayerShip() == ship && currentOverShield > 0){
             Global.getCombatEngine().maintainStatusForPlayerShip("vic_shieldHardeningPassive", "graphics/icons/hullsys/fortress_shield.png", "Absorption rate", Math.round(absorptionRate * 100f) + "%", false);
         }
     }
