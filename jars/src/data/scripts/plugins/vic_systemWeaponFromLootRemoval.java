f3dddcdd9d65647b804015158e183466eb72e1bc
PureTilt <kirostat@gmail.com>
2024-03-01 11:23:36 +0500

    fix for dropping build ins

--- jars/src/data/scripts/plugins/vic_systemWeaponFromLootRemoval.java
+++ jars/src/data/scripts/plugins/vic_systemWeaponFromLootRemoval.java
@@ -4,9 +4,11 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.BaseCampaignEventListener;
 import com.fs.starfarer.api.campaign.CargoAPI;
 import com.fs.starfarer.api.campaign.FleetEncounterContextPlugin;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.listeners.ShowLootListener;
 import com.fs.starfarer.api.combat.WeaponAPI;
 
-public class vic_systemWeaponFromLootRemoval extends BaseCampaignEventListener {
+public class vic_systemWeaponFromLootRemoval extends BaseCampaignEventListener implements ShowLootListener {
 
 
     public vic_systemWeaponFromLootRemoval(boolean permaRegister) {
@@ -17,8 +19,18 @@ public class vic_systemWeaponFromLootRemoval extends BaseCampaignEventListener {
         for (CargoAPI.CargoItemQuantity<String> weapon : loot.getWeapons()) {
             if (Global.getSettings().getWeaponSpec(weapon.getItem()).getAIHints().contains(WeaponAPI.AIHints.SYSTEM)){
                 loot.removeWeapons(weapon.getItem(), weapon.getCount());
+                Global.getLogger(vic_systemWeaponFromLootRemoval.class).info("removed " + weapon.getItem());
             }
         }
     }
 
+    @Override
+    public void reportAboutToShowLootToPlayer(CargoAPI loot, InteractionDialogAPI dialog) {
+        for (CargoAPI.CargoItemQuantity<String> weapon : loot.getWeapons()) {
+            if (Global.getSettings().getWeaponSpec(weapon.getItem()).getAIHints().contains(WeaponAPI.AIHints.SYSTEM)){
+                loot.removeWeapons(weapon.getItem(), weapon.getCount());
+                Global.getLogger(vic_systemWeaponFromLootRemoval.class).info("removed " + weapon.getItem());
+            }
+        }
+    }
 }
